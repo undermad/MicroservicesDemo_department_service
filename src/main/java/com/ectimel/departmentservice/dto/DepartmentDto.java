bad4cff0c866f06a75233786a6dfd717d7d106ef
Ectimel <dtworek94@gmail.com>
2023-05-16 01:21:46 +0100

    fixed type error in DepartmetDto class

--- src/main/java/com/ectimel/departmentservice/dto/DepartmentDto.java
+++ src/main/java/com/ectimel/departmentservice/dto/DepartmentDto.java
@@ -7,6 +7,7 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 public class DepartmentDto {
+    private long id;
     private String department;
     private String departmentDescription;
     private String departmentCode;
