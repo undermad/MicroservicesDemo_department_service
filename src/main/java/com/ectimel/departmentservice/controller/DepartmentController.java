9c60e2d5b94c4104eb397ca09474fc91ebae82db
Ectimel <dtworek94@gmail.com>
2023-05-20 01:01:35 +0100

    fixed typo error in controller endpoint. from department to departments

--- src/main/java/com/ectimel/departmentservice/controller/DepartmentController.java
+++ src/main/java/com/ectimel/departmentservice/controller/DepartmentController.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("api/v1/department")
+@RequestMapping("/api/v1/departments")
 public class DepartmentController {
 
     private DepartmentService departmentService;
